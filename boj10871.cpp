5d22c0ef62848002217a5e741f0767ceb3789452
Taeyeong <tyeong1102@gmail.com>
2022-06-24 15:54:18 +0900

    Create boj10871.cpp

---
+++ boj10871.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N, X;
+
+    cin >> N >> X;
+    
+    int arr[10000];
+     
+    for(int i=0; i<N; i++){
+        cin >> arr[i];
+
+        if(arr[i]<X){
+            cout << arr[i] << " ";
+        }
+    }
+}
