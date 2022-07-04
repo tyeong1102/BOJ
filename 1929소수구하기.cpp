326f6be02599ccbf1001c6462d7f56dcd3307abf
Taeyeong <tyeong1102@gmail.com>
2022-07-05 01:12:49 +0900

    Update 1929소수구하기.cpp

--- 1929소수구하기.cpp
+++ 1929소수구하기.cpp
@@ -10,7 +10,7 @@ void isPrime1(int m, int n){
     for(int i=2; i<=maxNum; i++){ 
         // 처음에는 i=0이라고 설정하고 해서 89%에서 오류가 남
         // 위에서 0과 1을 초기화 했지만 이 코드에서 isPrime[0]과 isPrime[1]을 다시 만들었기 때문에 오류
-        //i의 시작 범위를 2로 해서 해결
+        // i의 시작 범위를 2로 해서 해결
         isPrime[i] = i;
     }
 
