5aa7bb401aa3a6fbbed8df3d2bf611ebf6f2165d
Taeyeong <tyeong1102@gmail.com>
2022-07-06 16:30:51 +0900

    Create 4101_크냐.cpp

---
+++ 4101_크냐.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    
+
+    while(true){
+        cin >> a >> b;
+
+        if(a==0 && b==0){
+            break;
+        }
+
+        if(a > b){
+            cout << "Yes" << '\n';
+        } else{
+            cout << "No" << '\n';
+        }
+    }   
+}
