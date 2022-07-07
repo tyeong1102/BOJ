318dfc502a716cdf0017863649285266371bef8c
Taeyeong <tyeong1102@gmail.com>
2022-07-07 11:52:03 +0900

    Create 10171_고양이.cpp

---
+++ 10171_고양이.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// 큰 따옴표 안에 문자를 그대로 출력하려면 앞에 \를 붙혀야함.
+int main(){
+    cout << "\\    /\\\n";
+    cout << " )  ( ')\n";
+    cout << "(  /  )\n";
+    cout << " \\(__)|\n";
+}
