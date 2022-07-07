505b5ad035ac5c98f7048062c6ac760a42c69867
Taeyeong <tyeong1102@gmail.com>
2022-07-07 11:58:28 +0900

    Create 10172_개.cpp

---
+++ 10172_개.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+// \" 이렇게 출력해야함
+int main(){
+    cout << "|\\_/|\n";
+    cout << "|q p|   /}\n";
+    cout << "( 0 )\"\"\"\\\n";
+    cout << "|\"^\"`    |\n";
+    cout << "||_/=\\\\__|\n";
+}
