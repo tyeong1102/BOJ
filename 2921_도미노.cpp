a81b6550ee759f08fe87e96c2bc4e64bedd1d370
Taeyeong <tyeong1102@gmail.com>
2022-07-12 22:55:52 +0900

    Create 2921_도미노.cpp

---
+++ 2921_도미노.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    int cnt = 0;
+    cin >> n;
+
+    for(int i=0; i<=n; i++){
+        for(int j=0; j<=i; j++){
+            cnt += i + j;
+        }
+    }
+
+    cout << cnt << '\n';
+}
