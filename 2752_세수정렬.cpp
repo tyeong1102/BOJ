dd4eda832f839e1a561483793cce5e72e99cc617
Taeyeong <tyeong1102@gmail.com>
2022-07-19 12:29:43 +0900

    Create 2752_세수정렬.cpp

---
+++ 2752_세수정렬.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    vector<int> v;
+
+    for(int i=0; i<3; i++){
+        cin >> n;
+        v.push_back(n);
+    }
+
+    sort(v.begin(), v.end());
+
+    for(int i=0; i<3; i++){
+        cout << v[i] << " ";
+    }
+}
