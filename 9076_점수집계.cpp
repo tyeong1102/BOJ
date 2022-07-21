e5421beee64e14680867a0df2015c21a3de0327f
Taeyeong <tyeong1102@gmail.com>
2022-07-22 01:05:33 +0900

    Create 9076_점수집계.cpp

---
+++ 9076_점수집계.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int t, score;
+    cin >> t;
+
+    for(int i=0; i<t; i++){
+        vector<int> v;
+        for(int j=0; j<5; j++){
+            cin >> score;
+            v.push_back(score);
+        }
+        sort(v.begin(), v.end());
+
+        if((v[3]-v[1]) >= 4){
+            cout << "KIN" << '\n';
+        } else{
+            cout << v[1]+v[2]+v[3] << '\n';
+        }
+    }
+}
