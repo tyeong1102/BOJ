6c28d47fa0c139c63467efc456c608f1b421bb63
Taeyeong <tyeong1102@gmail.com>
2022-07-28 23:51:12 +0900

    Update 1157_단어공부.cpp

--- 1157_단어공부.cpp
+++ 1157_단어공부.cpp
@@ -2,17 +2,18 @@
 #include <string>
 using namespace std;
 
+// 아스키 코드 적응
 int main(){
-    int arr[26] = {};
+    int arr[26] = {}; // 처음에 초기화를 해주지 않아서 오류가 남.
     int cnt = 0;
     string s;
     cin >> s;
 
     for(int i=0; i<s.size(); i++){
         if(s[i] < 97){
-            arr[s[i]-65]++;
+            arr[s[i]-65]++; // 대문자
         } else{
-            arr[s[i]-97]++;
+            arr[s[i]-97]++; // 소문자
         }
     }
 
